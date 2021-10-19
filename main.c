61f40c708dd90c50b4b65c20e5a501ba0b5fda21
Natalie Simtion <74635514+NatalieSimtion@users.noreply.github.com>
2021-10-19 02:53:31 +0000

    dict check

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 
 
 char * readline (FILE * fp) {
-  ssize_t linesize;
+  size_t linesize;
 
   int linelength;
   char * lineBuffer = NULL;
-  linesize = getline(char **)&lineBuffer, &linesize, fp);
+  linesize = getline((char **)&lineBuffer, &linesize, fp);
   //if eof is reached
   if(linesize == -1)
   return NULL;
@@ -48,4 +48,41 @@ void readDictionary(char * filename) {
   while ((line = readline(file)) != NULL) {
     
   }
+
+  //new node
+
+  Node * node = (Node *)malloc(sizeof(Node));
+  node->word = line;
+  node->next = NULL;
+
+  //prepend node to dictionary
+  if (dictionary == NULL)
+  dictionary = node;
+
+  else {
+    node ->next = dictionary;
+    dictionary = node;
+  }
+  fclose(file);
+
+
 }
+
+//check if word belongs to the dict
+
+int isDictionaryWord(char * word) {
+  Node * node = dictionary;
+
+  while(node !=NULL) {
+    if(strcmp(word, node->word) == 0 ) {
+      return 1;
+      //match found
+    }
+    node = node->next;
+  }
+  return 0;
+  //match not found
+}
+
+
+
